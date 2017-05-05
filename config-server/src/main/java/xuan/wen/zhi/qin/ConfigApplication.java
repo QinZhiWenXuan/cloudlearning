3773e04ea0a37a7d2acb00aaa5e298e790f2d200
QinZhiWenXuan <qinzhiwenxuan@gmail.com>
2017-05-05 16:12:16 +0800

    version : 1.001
    *) 修改配置文件乱码
    +) 新增zuul

--- config-server/src/main/java/xuan/wen/zhi/qin/ConfigApplication.java
+++ config-server/src/main/java/xuan/wen/zhi/qin/ConfigApplication.java
@@ -1,5 +1,6 @@
 package xuan.wen.zhi.qin;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -11,6 +12,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ConfigApplication.class).web(true).run(args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
---
+++ zuul-server/src/main/java/xuan/wen/zhi/qin/ZuulApplication.java
@@ -0,0 +1,18 @@
+package xuan.wen.zhi.qin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by qinzhiwenxuan on 2017/5/5.
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
